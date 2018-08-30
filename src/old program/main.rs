7827c1536b9167f04e9ce50a2a9803a20754847c
LothTikar <LothTikar@gmail.com>
2018-08-29 21:58:14 -0500

    halting on current program to switch back to GTK becaues of increacing complexity

--- src/old program/main.rs
+++ src/old program/main.rs
@@ -13,27 +13,332 @@ use rusttype::{point, Font, Scale};
 use std::env;
 use std::io::Read;
 
+const BUTTON_FILL_COLOR: (f32,f32,f32) = (0.6,0.6,0.6);
+const BUTTON_BORDER_COLOR: (f32,f32,f32) =(0.2,0.2,0.2);
+
+const BUTTON_FILL_COLOR_HOVER: (f32,f32,f32) =(0.75,0.75,0.75);
+const BUTTON_BORDER_COLOR_HOVER: (f32,f32,f32) =(0.3,0.3,0.3);
+
+const BUTTON_FILL_COLOR_PRESSED: (f32,f32,f32) =(0.5,0.5,0.5);
+const BUTTON_BORDER_PRESSED: (f32,f32,f32) =(0.0,0.0,0.0);
+
+const BUTTON_TEXT_COLOR: (f32,f32,f32) =(1.0,1.0,1.0);
+
+const BUTTON_BORDER_THICKNESS: (f32) = 2.0;
+
+trait Renderable {
+    fn render(&self, verts: &mut Vec<GLfloat>, window_size: (f32, f32));
+    fn width(&self) -> f32;
+    fn height(&self) -> f32;
+
+    fn size(&self) -> (f32, f32) {
+        (self.width(), self.height())
+    }
+}
+
+trait TextureUser {
+    fn required_width(&self) -> f32;
+    fn required_height(&self) -> f32;
+
+    fn required_size(&self) -> (f32, f32) {
+        (self.required_width(), self.required_height())
+    }
+
+    fn set_offset(&mut self, offset: (f32, f32));
+    fn get_offset(&self) -> (f32, f32);
+
+    fn update_texture(&self, texture: &mut RgbaImage);
+}
+
+trait Clickable {
+    fn update_state(&mut self, mouse_pos: (f32, f32), button_pressed: bool);
+    fn is_clicked(&self) -> bool;
+}
+
 struct Header {
     number_of_hints: i32,
     questions: Vec<String>,
-    rendered_questions: Vec<RgbaImage>,
 }
 
 struct Slide {
     image: Option<RgbaImage>,
     hints: Vec<String>,
     answers: Vec<String>,
-    rendered_hints: Vec<RgbaImage>,
-    rendered_answers: Vec<RgbaImage>,
 }
 
-struct SlideRenderInfo {
+struct CurrentSlideRenderInfo {
     image_offset: (f32, f32),
-    hint_offsets: Vec<(f32, f32)>,
-    answers_offsets: Vec<(f32, f32)>,
     right_offset: (f32, f32),
     wrong_offset: (f32, f32),
     continue_offset: (f32, f32),
+}
+
+struct TexturedButton {
+    position: (f32, f32),
+    texture: RgbaImage,
+    texture_offset: (f32, f32),
+    mouse_hover: bool,
+    mouse_down: bool,
+    clicked: bool,
+}
+
+impl TexturedButton{
+    fn new(position: (f32,f32),texture: RgbaImage,texture_offset:(f32,f32)) -> TexturedButton {
+        TexturedButton {
+            position: position,
+            texture: texture,
+            texture_offset: texture_offset,
+            mouse_down: false,
+            clicked: false,
+        }
+    }
+}
+
+impl Renderable for TexturedButton {
+    fn render(&self, verts: &mut Vec<GLfloat>, window_size: (f32, f32)) {
+        if self.mouse_hover {
+            if self.mouse_down {
+            add_colored_box((self.position.0 + BUTTON_BORDER_THICKNESS,self.position.1 + BUTTON_BORDER_THICKNESS),0.0,BUTTON_BORDER_COLOR_HOVER,(texture.width()-BUTTON_BORDER_THICKNESS,texture.height()-BUTTON_BORDER_THICKNESS),window_size,verts);
+        } else {
+
+        }
+        }
+    }
+    fn width(&self) -> f32 {
+        self.texture.width()
+    }
+    fn height(&self) -> f32{
+        self.texture.height()
+    }
+}
+
+impl TextureUser for TexturedButton {
+    fn required_width(&self) -> f32 {
+        self.texture.width() as f32
+    }
+
+    fn required_height(&self) -> f32 {
+        self.texture.height() as f32
+    }
+
+    fn set_offset(&mut self, offset: (f32, f32)) {
+        self.texture_offset = offset;
+    }
+
+    fn get_offset(&self) -> (f32, f32) {
+        self.texture_offset
+    }
+
+    fn update_texture(&self, texture: &mut RgbaImage) {
+        for (x, y, pixel) in self.texture.enumerate_pixels() {
+            texture.put_pixel(
+                self.texture_offset.0 as u32 + x,
+                self.texture_offset.1 as u32 + y,
+                *pixel,
+            );
+        }
+    }
+}
+
+impl Clickable for TexturedButton {
+    fn update_state(&mut self, mouse_pos: (f32, f32), button_pressed: bool) {
+        let lower_left_corner = (
+            self.position.0 + self.texture.width() as f32,
+            self.position.1 + self.texture.height() as f32,
+        );
+        self.mouse_hover = mouse_pos.0 > self.position.0
+            && mouse_pos.1 > self.position.1
+            && mouse_pos.0 < lower_left_corner.0
+            && mouse_pos.1 < lower_left_corner.1;
+        if self.mouse_hover {
+            if button_pressed {
+                self.mouse_down = true;
+            } else {
+                self.clicked = self.mouse_down;
+                self.mouse_down = false;
+            }
+        } else {
+            self.clicked = false;
+            self.mouse_down = false;
+        }
+    }
+
+    fn is_clicked(&self) -> bool {
+        self.clicked
+    }
+}
+
+#[derive(PartialEq)]
+enum QuestionStatus {
+    right,
+    wrong,
+    untouched,
+}
+
+struct Question {
+    question: RgbaImage,
+    correct_answer: TexturedButton,
+    wrong_answers: Vec<TexturedButton>,
+    status: QuestionStatus,
+}
+
+impl Renderable for Question {
+    fn render(&self, verts: &mut Vec<GLfloat>, window_size: (f32, f32)) {
+        add_textured_box()
+    }
+    fn width(&self) -> f32 {
+        let mut width = 0;
+        width = width.max(question.width());
+        width = width.max(correct_answer.width());
+        for b in wrong_answers.iter() {
+            width = width.max(b.width());
+        }
+        width * ((self.wrong_answers.size()) as f32 / 2.0) + 10.0
+    }
+    fn height(&self) -> f32 {
+        let mut height = 0;
+        height = width.max(question.height());
+        height = width.max(correct_answer.height());
+        for b in wrong_answers.iter() {
+            width = width.max(b.height());
+        }
+        height * 2.0 + 20.0
+    }
+
+    fn size(&self) -> (f32, f32) {
+        (self.width(), self.height())
+    }
+}
+
+impl Question {
+    fn new(question: String, correct_answer: String, incorrect_answers: Vec<String>, texture_offset: (f32,f32), font: &Font) -> Question{
+        let question_image = render_text(font,15.0,question.as_str());
+        let correct_image = render_text(font,15.0,correct_answer.as_str());
+        let mut wrong_images: Vec<RgbaImage> = Vec::new();
+        for s in incorrect_answers {
+            wrong_images.push( render_text(font,15.0,s.as_str()));
+        }
+        let mut question =  Question {
+            question: question_image,
+            correct_answer: correct_image,
+            wrong_answers: Vec<TexturedButton>,
+            status: QuestionStatus,
+        };
+    }
+}
+
+impl TextureUser for Question {
+    fn required_width(&self) -> f32 {
+        let mut width = self.correct_answer.required_width();
+        for b in self.wrong_answers.iter() {
+            width += b.required_width();
+        }
+        width
+    }
+    fn required_height(&self) -> f32 {
+        let mut height = self.correct_answer.required_height();
+        for b in self.wrong_answers.iter() {
+            height += height.max(b.required_height());
+        }
+        height
+    }
+
+    fn required_size(&self) -> (f32, f32) {
+        (self.required_width(), self.required_height())
+    }
+
+    fn set_offset(&mut self, offset: (f32, f32)) {
+        let mut x_offset = offset.0;
+        self.correct_answer.set_offset((x_offset,offset.1));
+        x_offset += self.correct_answer.required_width();
+        for b in wrong_answers.iter() {
+            self.correct_answer.set_offset((x_offset,offset.1));
+            x_offset += self.correct_answer.required_width();
+        }
+    }
+    fn get_offset(&self) -> (f32, f32) {
+        self.correct_answer.get_offset()
+    }
+
+    fn update_texture(&self, texture: &mut RgbaImage) {
+        correct_answer.update_texture(texture);
+        for b in wrong_answers.iter() {
+            b.update_texture(texture);
+        }
+    }
+}
+
+impl Clickable for Question {
+    fn update_state(&mut self, mouse_pos: (f32, f32), button_pressed: bool) {
+        self.correct_answer.update_state(mouse_pos, button_pressed);
+        for b in self.wrong_answers.iter() {
+            b.update_state(mouse_pos, button_pressed);
+            if b.is_clicked() {
+                self.status = QuestionStatus::wrong;
+            }
+        }
+        if self.correct_answer.is_clicked() {
+            self.status = QuestionStatus::right;
+        }
+    }
+
+    fn is_clicked(&self) -> bool {
+        false
+    }
+}
+
+struct CurrentSlideInfo<'a> {
+    slide: &'a Slide,
+    render_info: CurrentSlideRenderInfo,
+    questions: Vec<Question>,
+}
+
+impl<'a> CurrentSlideInfo<'a> {
+    fn new(
+        slide: &Slide,
+        all_slides: &Vec<Slide>,
+        slide_offset: (f32, f32),
+        right_offset: (f32, f32),
+        wrong_offset: (f32, f32),
+        continue_offset: (f32, f32),
+    ) -> CurrentSlideInfo<'a> {
+        let render_info = CurrentSlideRenderInfo {
+            image_offset: slide_offset,
+            right_offset: right_offset,
+            wrong_offset: wrong_offset,
+            continue_offset: continue_offset,
+        };
+        let mut wrong_answers: Vec<Vec<String>> = Vec::new();
+        let mut rng = rand::thread_rng();
+        for category in 0..slide.answers.len() {
+            let mut random_answers = Vec::new();
+            while random_answers.len() < 3 {
+                let random_answer = rng
+                    .choose(all_slides)
+                    .expect("all_slides empty! Please fix!")
+                    .answers[category];
+                if random_answer != slide.answers[category] {
+                    random_answers.push(random_answer);
+                }
+            }
+            wrong_answers.push(random_answers);
+        }
+        CurrentSlideInfo {
+            slide: slide,
+            render_info: render_info,
+            questions: questions,
+        }
+    }
+
+    fn is_solved(&self) -> bool {
+        let mut solved = true;
+        for q in self.questions.iter() {
+            if q.status != QuestionStatus::right {
+                solved = false;
+            }
+        }
+        solved
+    }
 }
 
 fn add_colored_box(
@@ -444,40 +749,7 @@ fn render_text(font: &Font, scale: f32, text: &str) -> RgbaImage {
     image
 }
 
-fn main() {
-    let font_data = std::fs::read("./resources/Ubuntu-R.ttf").expect("Unable to open font file!");
-    let font = Font::from_bytes(font_data.as_slice()).expect("Error constructing Font");
-
-    let finish_text = render_text(&font, 32.0, "You've completed all questions!");
-
-    let mut csv_reader = {
-        let args: Vec<String> = env::args().collect();
-        csv::Reader::from_path(&args[1]).expect("Quiz file processing error!")
-    };
-
-    let mut header = Header {
-        number_of_hints: 0,
-        questions: Vec::new(),
-        rendered_questions: Vec::new(),
-    };
-
-    //It expects the first item to be "image file name." Hints are expected to only be placed immedently after "image file name." Everything after hints is expected to be a question.
-    for header_item in csv_reader.headers().unwrap().iter() {
-        let header_item = header_item.to_string();
-        match header_item.as_str() {
-            "hint" => header.number_of_hints += 1,
-            "image file name" => (),
-            _ => {
-                header
-                    .rendered_questions
-                    .push(render_text(&font, 15.0, header_item.as_str()));
-                header.questions.push(header_item);
-            }
-        }
-    }
-
-    let header = header;
-
+fn read_slides(csv_reader: &mut csv::Reader<std::fs::File>, header: &Header) -> Vec<Slide> {
     let mut slides: Vec<Slide> = Vec::new();
 
     for record in csv_reader.records() {
@@ -487,8 +759,6 @@ fn main() {
             image: None,
             hints: Vec::new(),
             answers: Vec::new(),
-            rendered_hints: Vec::new(),
-            rendered_answers: Vec::new(),
         };
 
         let mut i = 0;
@@ -505,15 +775,9 @@ fn main() {
                     }
                 }
                 _ if i <= header.number_of_hints => {
-                    slide
-                        .rendered_hints
-                        .push(render_text(&font, 15.0, value.as_str()));
                     slide.hints.push(value);
                 }
                 _ => {
-                    slide
-                        .rendered_answers
-                        .push(render_text(&font, 15.0, value.as_str()));
                     slide.answers.push(value);
                 }
             }
@@ -522,9 +786,54 @@ fn main() {
         slides.push(slide);
     }
 
-    thread_rng().shuffle(&mut slides);
+    slides
+}
 
-    let slides = slides;
+fn read_header(csv_reader: &mut csv::Reader<std::fs::File>) -> Header {
+    let mut header = Header {
+        number_of_hints: 0,
+        questions: Vec::new(),
+    };
+
+    //It expects the first item to be "image file name." Hints are expected to only be placed immedently after "image file name." Everything after hints is expected to be a question.
+    for header_item in csv_reader.headers().unwrap().iter() {
+        let header_item = header_item.to_string();
+        match header_item.as_str() {
+            "hint" => header.number_of_hints += 1,
+            "image file name" => (),
+            _ => {
+                header.questions.push(header_item);
+            }
+        }
+    }
+
+    header
+}
+
+fn main() {
+    let font_data = std::fs::read("./resources/Ubuntu-R.ttf").expect("Unable to open font file!");
+    let font = Font::from_bytes(font_data.as_slice()).expect("Error constructing Font");
+
+    let finish_text = render_text(&font, 32.0, "You've completed all questions!");
+
+    let mut csv_reader = {
+        let args: Vec<String> = env::args().collect();
+        csv::Reader::from_path(
+            &args
+                .get(1)
+                .expect("No argument provided for quiz filename!"),
+        ).expect("Quiz file processing error!")
+    };
+
+    let header = read_header(&mut csv_reader);
+
+    let slides = {
+        let mut slides = read_slides(&mut csv_reader, &header);
+
+        thread_rng().shuffle(&mut slides);
+
+        slides
+    };
 
     let img_right = image::open("./resources/icons8-checked-50.png").unwrap();
     let img_wrong = image::open("./resources/icons8-cancel-50.png").unwrap();
@@ -561,7 +870,7 @@ fn main() {
 
     let mut test_offset = (0.0, 0.0);
     let mut test_size = (0.0, 0.0);
-
+    /*
     let texture = {
         let mut texture_width = 0;
         let mut texture_height = 0;
@@ -592,20 +901,18 @@ fn main() {
         }
         image
     };
-
+*/
     unsafe {
         opengl_setup();
         setup_shaders(&vert_src, &frag_src);
         vertex_buffer_setup(&mut vert_buffer);
         texture_setup(&mut texture_buffer);
-        set_texture_data(&texture);
     }
 
     print_gl_error();
 
     let mut slides_iter = slides.iter();
-    let mut current_slide = slides_iter.next();
-    let mut correct_answers: Vec<bool> = Vec::new();
+    let mut current_slide;
 
     while !window.should_close() {
         let window_size = window.get_size();
@@ -619,19 +926,9 @@ fn main() {
             )
         };
 
-        let mut move_to_next = true;
-
-        for i in correct_answers {
-            if (!i) {
-                move_to_next = false;
-            }
-        }
-
         if move_to_next {
             current_slide = slides_iter.next();
-            if let Some(ref current_slide) = current_slide {
-				
-			}
+            if let Some(ref current_slide) = current_slide {}
         }
 
         let mut verts: Vec<GLfloat> = Vec::new();
@@ -660,7 +957,7 @@ fn main() {
                 set_texture_data(&finish_text);
             }
         }
-
+        /*
         add_colored_box(
             cursor_pos,
             0.5,
@@ -689,7 +986,7 @@ fn main() {
             (texture.width() as f32, texture.height() as f32),
             &mut verts,
         );
-
+*/
         unsafe {
             set_vertex_data(&verts);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
